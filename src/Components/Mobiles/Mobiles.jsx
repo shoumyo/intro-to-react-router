89865d7399505c8f26f1cf77dde6acc32b372dd5
Shoumyo <shoumyosrijito@gmail.com>
2026-06-03 23:01:56 +0600

    Routing concept

---
+++ src/Components/Mobiles/Mobiles.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mobiles = () => {
+    return (
+        <div>
+            <h2>This is Mobile</h2>
+        </div>
+    );
+};
+
+export default Mobiles;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Root from './Components/Root/Root.jsx'
 
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Home from './Components/Home/Home.jsx'
+import Mobiles from './Components/Mobiles/Mobiles.jsx'
+import Laptops from './Components/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component:Root,
+    children:[
+      {index:true,Component:Home},
+      {path:'Mobiles',Component:Mobiles },
+      {path:'laptops',Component:Laptops}
+    ]
   },
   {
     path:'about',
