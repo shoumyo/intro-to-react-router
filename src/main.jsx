8e670bf30655e58b20c93442400cf7def78d50f6
Shoumyo <shoumyosrijito@gmail.com>
2026-06-03 21:56:31 +0600

    react routing

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path:'about',
+    element:<div>About me</div>
+  },
+  {
+    path:'app',
+    Component:App
+  },
+  {
+    path:'app2',
+    element:<App></App>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
