5fc05dd8d8761e82249d974a89fc5f13e9b5b873
Shoumyo <shoumyosrijito@gmail.com>
2026-06-05 18:12:08 +0600

    Add navigation to home in User component; implement visitHome state management

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,9 +1,12 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
+import { Navigate } from 'react-router';
 
 const User = ({user}) => {
     const[showInfo,setShowInfo]=useState(false);
+    const[visitHome,setvisitHome]=useState(false);
+    const location=useLocation();
     const {id,name,email,phone}=user;
 
     const userPromise=fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res=>res.json());
@@ -14,6 +17,11 @@ const User = ({user}) => {
         padding:'10px',
         Margin:'10px'
     }
+
+    if(visitHome){
+        return <Navigate to='/'></Navigate>
+    }
+
     return (
         <div style={userStyle}>
             <h3>{name}</h3>
@@ -26,6 +34,7 @@ const User = ({user}) => {
                     <UserDetails2 userPromise={userPromise}></UserDetails2>
                 </Suspense>
             }
+            <button onClick={()=>setvisitHome(true)}>Visit Home</button>
         </div>
     );
 };
